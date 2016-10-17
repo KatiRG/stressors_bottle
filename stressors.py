209e58135dacae60b895044629ec7517df3ce7ad
katiRG <cnangini@gmail.com>
2016-10-17 15:05:51 +0200

    WIP: using template for view

--- stressors.py
+++ stressors.py
@@ -1,8 +1,9 @@
 import sqlite3
-from bottle import Bottle, route, run, debug, template, request, static_file
+from bottle import Bottle, route, run, debug, template, request, static_file, SimpleTemplate
 
 app = Bottle()
 
+#-------------------------------
 #static files for css
 @app.route('/static/<filename>')
 def server_static(filename):
@@ -13,10 +14,13 @@ def server_static(filename):
 def send_image(filename):
     return static_file(filename, root='images/', mimetype='image/jpg')
 
+#-------------------------------
+
 #http://pwp.stevecassidy.net/bottle/forms-processing.html
 @app.route('/')
 def index():
-    return template("scenario_form.tpl", message="", scenario="")
+    #return template("scenario_form.tpl", message="", scenario="")
+    return template("template", message="", scenario="")
 
 @app.route('/', method='POST')
 def formhandler():
@@ -24,12 +28,23 @@ def formhandler():
     
     message = "Showing maps for scenario " + scenario + ":"    
     
-    return template("scenario_form.tpl", message=message, scenario=scenario) 
+    return template("template.tpl", message=message, scenario=scenario)    
 
+# @app.route('/', method='POST')
+# def formhandler():
+#     scenario = request.forms.get('scenario')
+    
+#     message = "Showing maps for scenario " + scenario + ":"    
+    
+#     return template("scenario_form.tpl", message=message, scenario=scenario)
+
+
+#-------------------------------
 @app.route('/help')
 def help():
 	return static_file('help.html', root='.')
 
+#-------------------------------
 # # check against case where someone sends data without going through HTML form
 # def validate_form(form, required):
 # 	scenario = request.forms.get('scenario')
